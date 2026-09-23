fd1e3e6d2dd65e47018c376e9528b820c4c8f823
best-digital-offer <dream11sayee@gmail.com>
2026-09-23 12:03:57 +0530

    Restrict API CORS to KDP Digger deployment and remove legacy auth header

--- api/index.ts
+++ api/index.ts
@@ -8,9 +8,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS headers
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://kdpdigger.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-user-id');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
